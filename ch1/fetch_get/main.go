5bba80dc50a3f2d73c9e0e4d27a333124d931398
CaicoLeung <lkk1053190902@gmail.com>
2021-05-29 00:18:23 +0800

    fix: 输入的url参数没有 https:// 前缀的话，为这个url加上该前缀

--- ch1/fetch_get/main.go
+++ ch1/fetch_get/main.go
@@ -5,10 +5,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		// 如果输入的url参数没有 https:// 前缀的话，为这个url加上该前缀
+		if !strings.HasPrefix(url, "https://") && !strings.HasPrefix(url, "http://") {
+			url = "https://" + url
+		}
+		fmt.Printf("url: %s\n", url)
 		res, err := http.Get(url)
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "fetch_post: %v\n", err)
