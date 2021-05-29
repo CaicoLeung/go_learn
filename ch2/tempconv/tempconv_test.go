c57879c1fe146c70bd8da4d934e8db23ce2757c9
CaicoLeung <lkk1053190902@gmail.com>
2021-05-30 00:49:14 +0800

    test: tempconv_test

--- ch2/tempconv/tempconv_test.go
+++ ch2/tempconv/tempconv_test.go
@@ -1,7 +1,24 @@
-package tempconv0
+package tempconv
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-func Example_one()  {
-	fmt.Printf("%g\n", c,)
+/* go test -v ./ch2/tempconv/ -test.run TestExampleOne */
+func TestExampleOne(t *testing.T) {
+	fmt.Printf("%g\n", BoilingC-FreezingC)
+	boilingF := C2F(BoilingC)
+	fmt.Printf("%g\n", boilingF-C2F(FreezingC))
+	fmt.Printf("%g\n", C2F(AbsoluteZeroC))
+}
+
+func TestExampleTwo(t *testing.T)  {
+	c := F2C(212.0)
+	fmt.Println(c.String())
+	fmt.Printf("%v\n", c) // 不用显式调用String()
+	fmt.Printf("%s\n", c) // 不用显式调用String()
+	fmt.Printf("%g\n", c) // 不用显式调用String()
+	fmt.Println(c)
+	fmt.Println(float64(c))
 }
